341fd0ed6082a3efd52546a8e7410a3b8a6ce83f
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-04-29 20:25:46 +0300

    bitti

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,77 @@
-const checkCarId = (req, res, next) => {
-  // HOKUS POKUS
-}
+const carModel = require("./cars-model");
+const vinValidator = require("vin-validator");
+const checkCarId = async (req, res, next) => {
+  try {
+    const carId = req.params.id;
+    const car = await carModel.getById(carId);
+    if (car) {
+      req.car = car;
+      next();
+    } else {
+      res
+        .status(404)
+        .json({ mesaj: `${carId} kimliğine sahip araba bulunamadı` });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
-  // HOKUS POKUS
-}
+  try {
+    const { vin, make, model, mileage } = req.body;
+    if (vin) {
+      if (make) {
+        if (model) {
+          if (mileage) {
+            next();
+          } else {
+            res.status(400).json({ message: `mileage is missing` });
+          }
+        } else {
+          res.status(400).json({ message: `model is missing` });
+        }
+      } else {
+        res.status(400).json({ message: `make is missing` });
+      }
+    } else {
+      res.status(400).json({ message: `vin is missing` });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // HOKUS POKUS
-}
+  try {
+    const vin = req.body.vin;
+    if (vinValidator.validate(vin)) {
+      next();
+    } else {
+      res.status(400).json({ message: `vin ${vin} is invalid` });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-const checkVinNumberUnique = (req, res, next) => {
-  // HOKUS POKUS
-}
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+    const vin = req.body.vin;
+    const allCars = await carModel.getAll();
+    const car = allCars.find((car) => car.vin === vin);
+    if (car) {
+      res.status(400).json({ message: `vin ${vin} already exists` });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,37 @@
-// HOKUS POKUS
+const express = require("express");
+const router = express.Router();
+const carModel = require("./cars-model");
+const carMiddleware = require("./cars-middleware");
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await carModel.getAll();
+    res.status(200).json(cars);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:id", carMiddleware.checkCarId, async (req, res, next) => {
+  try {
+    const car = req.car;
+
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
+});
+router.post(
+  "/",
+  carMiddleware.checkCarPayload,
+  carMiddleware.checkVinNumberValid,
+  carMiddleware.checkVinNumberUnique,
+  async (req, res, next) => {
+    try {
+      const newCar = await carModel.create(req.body);
+      res.status(201).json(newCar);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
